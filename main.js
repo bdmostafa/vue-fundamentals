c15144ba371be35da8862a88d818a02adc7fd43b
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-07 13:27:15 +0600

    props pass from html to main.js

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+Vue.component('message', {
 
+    props: ['title', 'body'],
+
+    data () {
+        return {
+            inVisible: true,
+        }
+    },
+
+    template: `
+        <article class="message" v-show="inVisible">
+            <div class="message-header">
+                <p>{{title}}</p>
+            <button class="delete" aria-label="delete" @click="inVisible = false"></button>
+            </div>
+            <div class="message-body">
+                {{body}}
+            </div>
+        </article> 
+    `,
+
+    methods: {
+        // hide () {
+        //     this.inVisible = false;
+        // }
+    }
+});
 
 
 new Vue({
