bb8542138b25f2ed916df52f8fcd9a3f85096417
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-07 12:47:03 +0600

    bulma css added

--- main.js
+++ main.js
@@ -1,39 +1,5 @@
-Vue.component('task-list', {
-    template: `
-            <div>
-                <h2>All Tasks</h2>
-                <task v-for="task in tasks">
-                    {{task.task}}
-                </task>
-                <hr />
-                <h2>Completed Tasks</h2>
-                <task v-for="task in tasks" v-if="task.completed">
-                    {{task.task}}
-                </task>
-                <h2>Uncompleted Tasks</h2>
-                <task v-for="task in tasks" v-if="!task.completed">
-                    {{task.task}}
-                </task>
-            </div>
-            
-        `,
 
-    data () {
-        return {
-            tasks : [
-                {task: 'go to office', completed: false},
-                {task: 'go to bed', completed: true},
-                {task: 'go back home', completed: true},
-                {task: 'go to kitchen', completed: false},
-                {task: 'go to field', completed: true},
-            ],
-        }
-    }
-})
 
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
-})
 
 new Vue({
     el:'#app-root'
