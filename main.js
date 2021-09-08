8e127fdeb86ac750d1567b1926401360b3c40a06
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-08 18:45:48 +0600

    one way communication between parant and child with custom event applied

--- main.js
+++ main.js
@@ -1,67 +1,25 @@
-Vue.component("tabs", {
-  template: `
-        <div>
-            <div class="tabs is-centered">
-                <ul>
-                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }"> 
-                        <a :href="tab.href" @click="selectTab(tab)"> {{ tab.name }} </a> 
-                    </li>
-                </ul>
-            </div>
+Vue.component("coupon", {
 
-            <div class="tabs-details">
-                <slot></slot>
-            </div>
-        </div>
-    `,
+  template: '<input placeholder="Enter the coupon code" @blur="onCouponApplied">',
 
-  mounted() {
-    console.log(this.tabs);
+  methods: {
+    onCouponApplied() {
+      this.$emit('applied');
+    },
   },
+});
 
-  data() {
-    return { tabs: [] };
-  },
+new Vue({
+  el: "#app-root",
 
-  created() {
-    this.tabs = this.$children;
+  data: {
+      couponApplied: false,
   },
 
   methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach((tab) => {
-        tab.isActive = tab.name === selectedTab.name;
-      });
+    onCouponApplied() {
+      alert("applied successfully........");
+      this.couponApplied = true;
     },
   },
-}),
-  Vue.component("tab", {
-    template: `
-        <div v-show="isActive"><slot></slot></div>
-    `,
-
-    props: {
-      name: { required: true },
-
-      selected: { default: false },
-    },
-
-    data() {
-      return {
-        isActive: false,
-      };
-    },
-
-    mounted() {
-      this.isActive = this.selected;
-    },
-
-    computed: {
-      href() {
-        return "#" + this.name.toLowerCase().replace(/ /g, "-");
-      },
-    },
-  }),
-  new Vue({
-    el: "#app-root",
-  });
+});
