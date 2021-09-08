fb47ce15d0a80fbf7d5088d2f66a4582f5d20d21
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-08 15:43:24 +0600

    components tab usages dynamically

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
-Vue.component('message', {
-
-    props: ['title', 'body'],
-
-    data () {
-        return {
-            inVisible: true,
-        }
-    },
-
-    template: `
-        <article class="message" v-show="inVisible">
-            <div class="message-header">
-                <p>{{title}}</p>
-            <button class="delete" aria-label="delete" @click="inVisible = false"></button>
+Vue.component("tabs", {
+  template: `
+        <div>
+            <div class="tabs is-centered">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }"> 
+                        <a :href="tab.href" @click="selectTab(tab)"> {{ tab.name }} </a> 
+                    </li>
+                </ul>
             </div>
-            <div class="message-body">
-                {{body}}
+
+            <div class="tabs-details">
+                <slot></slot>
             </div>
-        </article> 
-    `,
-
-    methods: {
-        // hide () {
-        //     this.inVisible = false;
-        // }
-    }
-});
-
-Vue.component('modal', {
-    
-    props: ['body'],
-
-    template: `
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-                <div class="modal-content">
-                    <div class="box">
-                        <p> {{ body }} </p>
-                    </div>
-                </div>
-            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
         </div>
     `,
-})
 
+  mounted() {
+    console.log(this.tabs);
+  },
 
-new Vue({
-    el:'#app-root',
+  data() {
+    return { tabs: [] };
+  },
 
-    data () {
-        return {
-            showModal: false,
-        }
-    }
-})
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name === selectedTab.name;
+      });
+    },
+  },
+}),
+  Vue.component("tab", {
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+
+    props: {
+      name: { required: true },
+
+      selected: { default: false },
+    },
+
+    data() {
+      return {
+        isActive: false,
+      };
+    },
+
+    mounted() {
+      this.isActive = this.selected;
+    },
+
+    computed: {
+      href() {
+        return "#" + this.name.toLowerCase().replace(/ /g, "-");
+      },
+    },
+  }),
+  new Vue({
+    el: "#app-root",
+  });
