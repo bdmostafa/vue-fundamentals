30239be1bd489a5a08a9049f0a2481177caaf104
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-09 20:06:51 +0600

    slots usages in modal header, content and footer section

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-Vue.component("progress-view", {
-  
-  data() {
-    return {completionRate: 10};
-  },
-
-
+Vue.component("modal", {
+  template: `
+    <div style="padding: 50px;" class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">
+         <slot name="header"></slot>
+        </p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <slot name="content"></slot>
+      </section>
+      <footer class="modal-card-foot">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  </div>
+  `,
 });
 
 new Vue({
